a979ed10331eb24dd21eff0dff306c5c68a84819
Habibtusher <tusherhrt@gmail.com>
2023-11-26 22:07:34 +0600

    recursive factorial

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fibonacci = (n) => {
   }
   return fib;
 };
-// console.log(fibonacci(2));
+console.log(fibonacci(12));
 
 const factorial = (n) => {
   let result = 1;
@@ -14,18 +14,24 @@ const factorial = (n) => {
   }
   return result;
 };
-// console.log(factorial(5));
+const refactorial = (n) => {
+  if (n === 0) {
+    return 1
+  }
+  return n * refactorial(n - 1)
+};
+console.log(refactorial(5));
 
 const checkPrime = (n) => {
-    if(n < 2){
-        return false;
+  if (n < 2) {
+    return false;
+  }
+  for (let i = 0; i < n; i++) {
+    if (n % 2 === 0) {
+      return false;
     }
-    for (let i = 0; i < n; i++) {
-        if(n % 2 === 0){
-            return false;
-        }
-    }
-    return true;
+  }
+  return true;
 
 };
 console.log(checkPrime(4)); 
