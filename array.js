a21ff62e5c57a23d2db7da21d38cf697f96a5304
Habibtusher <tusherhrt@gmail.com>
2024-01-15 19:49:52 +0600

    smallerNumbersThanCurrent

--- array.js
+++ array.js
@@ -37,7 +37,7 @@ var smallerNumbersThanCurrent = function (nums) {
   for (let i = 0; i < nums.length; i++) {
     let count = 0;
     for (let j = 0; j < nums.length; j++) {
-      if (i !== j && nums[i] > nums[j]) {
+      if (nums[i] > nums[j]) {
         count++;
       }
     }
