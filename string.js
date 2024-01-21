d71608285cf5a5963fecde930321155140f6fb4e
Habibtusher <tusherhrt@gmail.com>
2024-01-21 10:07:40 +0600

    maximumWealth

--- string.js
+++ string.js
@@ -14,11 +14,22 @@ var mostWordsFound = function (sentences) {
     let maxLength = 0;
     for (let i = 0; i < sentences.length; i++) {
         const sentence = sentences[i].split(' ');
-        if(sentence.length > maxLength){
+        if (sentence.length > maxLength) {
             maxLength = sentence.length;
         }
 
     }
     return maxLength;
 }
-console.log(mostWordsFound(["alice and bob love leetcode", "i think so too", "this is great thanks very much"]));
+var maximumWealth = function (accounts) {
+    // let result = 0
+    // for (let i = 0; i < accounts.length; i++) {
+    //     const element = accounts[i].reduce((acc, account) => acc + account)
+    //     if (element > result) result = element
+
+    // }
+    // return result
+    return Math.max(...accounts.map((e,) => e.reduce((sum, balance) => sum + balance, 0)))
+};
+console.log(maximumWealth([[1, 5], [7, 3], [3, 5]]));
+// console.log(mostWordsFound(["alice and bob love leetcode", "i think so too", "this is great thanks very much"]));
